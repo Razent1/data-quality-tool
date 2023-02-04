fce724db8817dee47bc6be20a2b455375c5ce8ec
ilya <irozent@whoisblogger.com>
2023-02-04 22:27:35 +0200

    bash

--- start.sh
+++ start.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/bash
 
-#export SERVER_HOST=<databricks-host>
-#export HTTP_PATH=<databricks-http-path>
-#export NOTEBOOK_PATH=<databricks-notebook-path>
-#export CLUSTER_ID=<databricks-cluster-id>
-#export RESULT_DATABASE=<databricks-result-database>
-#export RESULT_TABLE_NAME=<databricks-result-table>
-#export DATABRICKS_ACCOUNT_ID=<databricks-account-id>
-#export SLACK_CHANNEL_URL=<slack-channel-url>
-#export SLACK_CHANNEL_NAME=<slack-channel-name>
-#export JIRA_TOKEN=<jira-token>
-#export JIRA_URL=<jira-url>
-#export JIRA_PROJECT_ID=<jira-project-id>
+export SERVER_HOST=<databricks-host>
+export HTTP_PATH=<databricks-http-path>
+export NOTEBOOK_PATH=<databricks-notebook-path>
+export CLUSTER_ID=<databricks-cluster-id>
+export RESULT_DATABASE=<databricks-result-database>
+export RESULT_TABLE_NAME=<databricks-result-table>
+export DATABRICKS_ACCOUNT_ID=<databricks-account-id>
+export SLACK_CHANNEL_URL=<slack-channel-url>
+export SLACK_CHANNEL_NAME=<slack-channel-name>
+export JIRA_TOKEN=<jira-token>
+export JIRA_URL=<jira-url>
+export JIRA_PROJECT_ID=<jira-project-id>
 
 service="$1"
 
